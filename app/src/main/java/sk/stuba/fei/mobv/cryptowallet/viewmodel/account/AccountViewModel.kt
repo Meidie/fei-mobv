110a1a8920c7630bc846911bb6a026f68244a9ed
Keefko <kohooutdan@gmail.com>
2021-11-28 19:26:25 +0100

    AccountViewModel.kt Input Validation

--- app/src/main/java/sk/stuba/fei/mobv/cryptowallet/viewmodel/account/AccountViewModel.kt
+++ app/src/main/java/sk/stuba/fei/mobv/cryptowallet/viewmodel/account/AccountViewModel.kt
@@ -1,32 +1,41 @@
 package sk.stuba.fei.mobv.cryptowallet.viewmodel.account
 
-import android.text.TextUtils
-import android.widget.Button
-import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.fragment.NavHostFragment.findNavController
-import androidx.navigation.fragment.findNavController
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.stellar.sdk.KeyPair
 import retrofit2.Response
-import sk.stuba.fei.mobv.cryptowallet.R
 import sk.stuba.fei.mobv.cryptowallet.database.entity.Account
 import sk.stuba.fei.mobv.cryptowallet.database.entity.Balance
 import sk.stuba.fei.mobv.cryptowallet.database.entity.PinData
 import sk.stuba.fei.mobv.cryptowallet.repository.AccountRepository
 import sk.stuba.fei.mobv.cryptowallet.repository.BalanceRepository
 import sk.stuba.fei.mobv.cryptowallet.security.Crypto
+import sk.stuba.fei.mobv.cryptowallet.util.FormError
+
+
 
 class AccountViewModel(
     private val accountRepository: AccountRepository,
     private val balanceRepository: BalanceRepository
 ) : ViewModel() {
+
+    val pinError = ObservableField<FormError>()
+    val keyError = ObservableField<FormError>()
+
+
+    private val _keypair: MutableLiveData<ImportAccount> = MutableLiveData()
+    val keypair: LiveData<ImportAccount>
+        get() =  _keypair
+
+    private val _loadingResponse: MutableLiveData<String> = MutableLiveData()
+    val loadingResponse: LiveData<String>
+        get() = _loadingResponse
 
     private val _accountRegistrationResponse: MutableLiveData<Response<Void>> = MutableLiveData()
     val accountRegistrationResponse: LiveData<Response<Void>>
@@ -35,8 +44,10 @@ class AccountViewModel(
     private val _account: MutableLiveData<Account?> = MutableLiveData()
     val account: LiveData<Account?>
         get() = _account
-    init {
 
+    init {
+        pinError.set(FormError.NO_ERROR)
+        keyError.set(FormError.NO_ERROR)
     }
 
     fun update(account: Account) = viewModelScope.launch {
@@ -52,43 +63,84 @@ class AccountViewModel(
     }
 
 
-    fun login(key: String): Job = viewModelScope.launch {
-
-        val pair = KeyPair.fromSecretSeed(key);
-        val response: Account? = accountRepository.findByPublicKey(pair.accountId)
-        _account.postValue(response)
-    }
-
-
-    // TODO pridat progress bar v UI lebo to trva nejaku dobu
-    fun createAccount(pin: String) = viewModelScope.launch(Dispatchers.IO) {
-        val pair: KeyPair = KeyPair.random()
-        val response: Response<Void> = accountRepository.createAccount(pair.accountId)
-
-        if (response.isSuccessful) {
-
-            val accountResponse = accountRepository.getAccountInfo(pair.accountId)
-            if (accountResponse != null) {
-
-                val crypto = Crypto()
-                val cipherData = crypto.encrypt(String(pair.secretSeed), pin)
-                val hashedPin = crypto.secretKeyGenerator.generateSecretKey(pin)
-
-                val accId = accountRepository.insert(
-                    Account(
-                        0L, pair.accountId, cipherData,
-                        PinData(hashedPin.salt, hashedPin.secretKey.encoded),
-                        true
-                    )
-                )
-
-                for (balance in accountResponse.balances) {
-                    balanceRepository.insert(Balance(0L, accId, balance.assetType, balance.balance))
-                }
+    fun login(key: String?): Job = viewModelScope.launch {
+        when {
+            key.isNullOrEmpty() -> {
+                keyError.set(FormError.MISSING_VALUE)
+            }
+            key.length != 56 -> {
+                keyError.set(FormError.INVALID_PK_LENGTH)
+            }
+            else -> {
+                val pair = KeyPair.fromSecretSeed(key)
+                val response: Account? = accountRepository.findByPublicKey(pair.accountId)
+                _account.postValue(response)
             }
         }
-
-        _accountRegistrationResponse.postValue(response)
     }
 
+    fun createAccount(pin: String?) = viewModelScope.launch(Dispatchers.IO) {
+
+        when {
+            pin.isNullOrEmpty() -> {
+                pinError.set(FormError.MISSING_VALUE)
+            }
+            pin.length != 4 -> {
+                pinError.set(FormError.INVALID_PK_LENGTH)
+            }
+            else -> {
+                _loadingResponse.postValue("loading")
+                val pair: KeyPair = KeyPair.random()
+                val response: Response<Void> = accountRepository.createAccount(pair.accountId)
+                _keypair.postValue(ImportAccount(pair.accountId, pair.secretSeed.toString()))
+                if (response.isSuccessful) {
+                    insertAccount(pair, pin)
+                }
+
+                _accountRegistrationResponse.postValue(response)
+            }
+        }
+    }
+
+    private suspend fun insertAccount(pair: KeyPair, pin: String) {
+        val accountResponse = accountRepository.getAccountInfo(pair.accountId)
+        if (accountResponse != null) {
+
+            val crypto = Crypto()
+            val cipherData = crypto.encrypt(String(pair.secretSeed), pin)
+            val hashedPin = crypto.secretKeyGenerator.generateSecretKey(pin)
+
+            val accId = accountRepository.insert(
+                Account(
+                    0L, pair.accountId, cipherData,
+                    PinData(hashedPin.salt, hashedPin.secretKey.encoded),
+                    true
+                )
+            )
+
+            for (balance in accountResponse.balances) {
+                balanceRepository.insert(Balance(0L, accId, balance.assetType, balance.balance))
+            }
+        }
+    }
+
+
+    suspend fun import(pin: String?, privateKey: String) {
+
+        when {
+            pin.isNullOrEmpty() -> {
+                pinError.set(FormError.MISSING_VALUE)
+            }
+            pin.length != 4 -> {
+                pinError.set(FormError.INVALID_PK_LENGTH)
+            }
+            else -> {
+                val pair = KeyPair.fromSecretSeed(privateKey)
+                insertAccount(pair, pin)
+            }
+        }
+    }
+
+    class ImportAccount(val publicKey: String, val privateKey: String) {}
 }
+
