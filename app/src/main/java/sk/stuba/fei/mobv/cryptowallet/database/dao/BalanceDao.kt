97dae38575dbb8b04c9c49d5fa7faa2a50499aab
pekop777 <peter.kopan@gmail.com>
2021-11-29 22:18:37 +0100

    feat: updating balance from Stellar + getAmount

--- app/src/main/java/sk/stuba/fei/mobv/cryptowallet/database/dao/BalanceDao.kt
+++ app/src/main/java/sk/stuba/fei/mobv/cryptowallet/database/dao/BalanceDao.kt
@@ -16,4 +16,10 @@ interface BalanceDao : IDao<Balance> {
 
     @Query("SELECT * FROM balance_table")
     fun getAllBalances(): LiveData<List<Balance>>
+
+    @Query("UPDATE balance_table SET amount=:amount WHERE currency = :currency")
+    fun updateAmount(amount: String, currency: String)
+
+    @Query("SELECT amount FROM balance_table WHERE account_owner_id = ( select accountId from account_table where active_account) AND currency = :currency")
+    fun getAmount(currency: String): LiveData<String>
 }
