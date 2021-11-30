8f222802f8b87da82a9207bc4002b3296ea5f0a3
Matus Pohancenik <matuspohancenik@gmail.com>
2021-11-30 21:27:10 +0100

    feat: pk copy from transaction detail

--- app/src/main/java/sk/stuba/fei/mobv/cryptowallet/ui/fragment/transaction/TransactionDetailFragment.kt
+++ app/src/main/java/sk/stuba/fei/mobv/cryptowallet/ui/fragment/transaction/TransactionDetailFragment.kt
@@ -1,9 +1,13 @@
 package sk.stuba.fei.mobv.cryptowallet.ui.fragment.transaction
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import sk.stuba.fei.mobv.cryptowallet.R
@@ -29,6 +33,19 @@ class TransactionDetailFragment : Fragment() {
         binding.contact.setText(if (transaction.contact.name != "") transaction.contact.name else "Contact not saved")
         binding.image.setBackgroundResource(if (transaction.transaction.type.name.equals(TransactionType.CREDIT.name))
             R.drawable.ic_baseline_trending_up_green_24 else R.drawable.ic_baseline_trending_down_red_24)
+
+        binding.publicKeyLayout.setEndIconOnClickListener {
+            val pk = transaction.transaction.publicKey
+            val clipboardManager =
+                requireActivity().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("public key", pk)
+            clipboardManager.setPrimaryClip(clipData)
+            Toast.makeText(
+                requireContext(),
+                "Public key copied to clipboard",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
 
         return binding.root
     }
